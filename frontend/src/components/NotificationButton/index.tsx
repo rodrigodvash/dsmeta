18a5b5ab86d7712bba19bad70f6fcb466ad9d595
Rodrigo Maia <maiarodrigox@gmail.com>
2022-09-05 23:39:59 +0100

    useState

--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -1,6 +1,5 @@
-import icon from '../../assets/img/notification-icon.svg'
-
-import './styles.css'
+import icon from '../../assets/img/notification-icon.svg';
+import './styles.css';
 
 function NotificationButton() {
     return (
@@ -10,4 +9,4 @@ function NotificationButton() {
     )
 }
 
-export default NotificationButton
+export default NotificationButton;
