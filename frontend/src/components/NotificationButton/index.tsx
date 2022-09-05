ee8dc6b6d4ecba6970605644b1b74558ec74723c
Rodrigo Maia <maiarodrigox@gmail.com>
2022-09-05 21:53:33 +0100

    First Component

---
+++ frontend/src/components/NotificationButton/index.tsx
@@ -0,0 +1,13 @@
+import icon from '../../assets/img/notification-icon.svg'
+
+import './styles.css'
+
+function NotificationButton() {
+    return (
+        <div className="dsmeta-red-button">
+            <img src={icon} alt="Notificar" />
+        </div>
+    )
+}
+
+export default NotificationButton
